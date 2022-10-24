f943290afb7335b0c793313f4a75e3380153f713
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-10-24 23:54:56 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-     
+     <RouterProvider router={router}>
+
+     </RouterProvider>
     </div>
   );
 }
